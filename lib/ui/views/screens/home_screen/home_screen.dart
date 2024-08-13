44818657a98e6bc41b9bb388f0ebfa7e993b4753
golibjon <golibtoramurodov@gmail.com>
2024-08-13 16:21:03 +0500

    fsdhdf

--- lib/ui/views/screens/home_screen/home_screen.dart
+++ lib/ui/views/screens/home_screen/home_screen.dart
@@ -4,11 +4,13 @@ import 'package:flutter_application/logic/cubits/home_screen_cubits.dart';
 import 'package:flutter_application/ui/views/screens/home_screen/example.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _HomeScreenState createState() => _HomeScreenState();
 }
 
@@ -157,18 +159,111 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: ListView.builder(
                     itemCount: Example.ingredients.length,
                     itemBuilder: (context, index) {
-                      return Container(
-                        margin: EdgeInsets.symmetric(vertical: 15),
-                        height: 200,
-                        width: 335,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFFFF9B05),
-                          borderRadius: BorderRadius.circular(8),
-                          // image: DecorationImage(
-                          //     image: AssetImage(
-                          //         Example.ingredients[index].imageUrl),
-                          //     fit: BoxFit.cover),
-                        ),
+                      return Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 15),
+                            height: 250,
+                            width: double.maxFinite,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                    Example.ingredients[index].imageUrl),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    child: Image.asset(
+                                      'assets/images/share_icon.png',
+                                      height: 22,
+                                    ),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      CircleAvatar(
+                                        backgroundColor: Colors.white,
+                                        child: Image.asset(
+                                          'assets/images/comment_icon.png',
+                                          height: 22,
+                                          color: const Color(0xFFFF9B05),
+                                        ),
+                                      ),
+                                      GestureDetector(
+                                        onTap: () {},
+                                        child: Container(
+                                          alignment: Alignment.center,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadiusDirectional
+                                                    .circular(10),
+                                            color: Colors.white,
+                                          ),
+                                          width: 59,
+                                          height: 42,
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              const Icon(
+                                                Icons.favorite_border_outlined,
+                                                color: Color(0xFFFF9B05),
+                                                size: 25,
+                                              ),
+                                              Padding(
+                                                padding: const EdgeInsets.only(
+                                                    top: 8),
+                                                child: Text(
+                                                  Example
+                                                      .ingredients[index].likes
+                                                      .toString(),
+                                                  style: const TextStyle(
+                                                    fontSize: 10,
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                Example.ingredients[index].title,
+                                style: const TextStyle(
+                                  color: Color(0xFF1E1E1E),
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                'time: ${Example.ingredients[index].estimatedTime
+                                        .inMinutes}min',
+                                style: const TextStyle(
+                                  color: Color(0xFF1E1E1E),
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
                       );
                     },
                   ),
@@ -181,3 +276,23 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+// GestureDetector(
+//                                         onTap: () {},
+//                                         child: Container(
+//                                           alignment: Alignment.center,
+//                                           decoration: BoxDecoration(
+//                                             borderRadius:
+//                                                 BorderRadiusDirectional
+//                                                     .circular(10),
+//                                             color: Colors.white,
+//                                           ),
+//                                           width: 59,
+//                                           height: 42,
+//                                           child: Text(
+//                                             '${Example.ingredients[index].estimatedTime.inMinutes} min',
+//                                             style:
+//                                                 const TextStyle(fontSize: 12),
+//                                           ),
+//                                         ),
+//                                       ),
