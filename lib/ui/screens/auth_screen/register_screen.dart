d7c5d2ecb6cd9df62ff87097a48c2d11ae6696c3
golibjon <golibtoramurodov@gmail.com>
2024-08-13 15:00:55 +0500

    hjk

--- lib/ui/screens/auth_screen/register_screen.dart
+++ lib/ui/screens/auth_screen/register_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/logic/bloc/auth/auth_bloc.dart';
 import 'package:flutter_application/ui/screens/auth_screen/login_screen.dart';
-import 'package:flutter_application/ui/screens/home_screen.dart';
+import 'package:flutter_application/ui/views/screens/home_screen/home_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lottie/lottie.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
