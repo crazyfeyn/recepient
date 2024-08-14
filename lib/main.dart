053f985fef7b3f3896c071e1052f54be85a1faa7
Shoxrux Rasulov <shokhrukhrasulov07@gmail.com>
2024-08-14 20:47:14 +0500

    eferf

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/data/repositories/auth_repository.dart';
 import 'package:flutter_application/firebase_options.dart';
 import 'package:flutter_application/logic/bloc/auth/auth_bloc.dart';
-import 'package:flutter_application/logic/bloc/auth/auth_state.dart';
 import 'package:flutter_application/logic/bloc/home/home_bloc.dart';
 import 'package:flutter_application/logic/cubits/home_screen_cubits.dart';
 import 'package:flutter_application/ui/screens/navigationbar_screen/all_navigation_bar.dart';
-import 'package:flutter_application/ui/screens/splash_screens/welcome_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main(List<String> args) async {
@@ -47,13 +46,10 @@ class MyApp extends StatelessWidget {
               if (snapshot.hasData && snapshot.data != null) {
                 return const AllNavigationBar();
               } else {
-                // return const WelcomeScreen();
                 return const AllNavigationBar();
               }
             },
           ),
-
-          home: const WelcomeScreen(), // Always start with WelcomeScreen
         ),
       ),
     );
