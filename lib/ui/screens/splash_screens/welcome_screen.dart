c3e8648d05258c5f6f77c851aefe8b04ead9a06d
Jamshidmirzo <101593441+Jamshidmirzo@users.noreply.github.com>
2024-08-15 11:28:19 +0500

    done auth

--- lib/ui/screens/splash_screens/welcome_screen.dart
+++ lib/ui/screens/splash_screens/welcome_screen.dart
@@ -1,6 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_application/logic/bloc/auth/auth_event.dart';
+import 'package:flutter_application/ui/screens/auth_screen/login_screen.dart';
 import 'package:flutter_application/ui/screens/splash_screens/first_onboarding_screen.dart';
+import 'package:flutter_application/ui/screens/all_navigation_bar.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_application/logic/bloc/auth/auth_bloc.dart';
+import 'package:flutter_application/logic/bloc/auth/auth_state.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -13,37 +19,56 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 2), () {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<AuthBloc>().add(AppStartedEvent());
+    });
+  }
+
+  void _navigateBasedOnState(AuthState state) {
+    if (state is AuthAuthenticated) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const AllNavigationBar()),
+      );
+    } else if (state is AuthUnauthenticated) {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const FirstOnboardingScreen()),
       );
-    });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: Colors.orange.shade300,
-          image: const DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage(
-              'assets/images/splash/image.png',
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          // Navigate based on the authentication state after the delay
+          Future.delayed(const Duration(seconds: 2), () {
+            _navigateBasedOnState(state);
+          });
+        },
+        child: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.orange.shade300,
+            image: const DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                'assets/images/splash/image.png',
+              ),
             ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/splash/logo.png',
-              width: 150,
-              height: 150,
-            ),
-          ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/splash/logo.png',
+                width: 150,
+                height: 150,
+              ),
+            ],
+          ),
         ),
       ),
     );
